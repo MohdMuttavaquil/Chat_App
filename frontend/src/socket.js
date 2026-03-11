1af630f48e5c41574285acbffc8420b0865713f3
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-03-11 13:17:52 +0530

    Add url in socket.io

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const url = "http://localhost:3000"
-
+// const url = "http://localhost:3000"
+  const url = "https://chat-app-jkuj.onrender.com"
 export const socket  = io(url, { autoConnect: false})
