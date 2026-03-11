9f20fa9877bf576420d79e9997194282df4c4446
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-03-11 13:12:49 +0530

    Add url on axios instance

--- frontend/src/Api/axiosinstance.js
+++ frontend/src/Api/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://chat-app-jkuj.onrender.com/api",
 })
 
 export default axiosInstance
